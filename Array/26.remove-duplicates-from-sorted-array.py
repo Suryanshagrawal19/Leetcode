9a6ac812fc73f7ea2d37e392468a08379c732978
Suryanshagrawal19 <156478717+Suryanshagrawal19@users.noreply.github.com>
2025-05-11 18:38:03 -0400

    Remove Element

--- Array/26.remove-duplicates-from-sorted-array.py
+++ Array/26.remove-duplicates-from-sorted-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
         i=0
-        j=0
-        while i<len(nums):
+        j=1
+        while i<len(nums) and j<len(nums):
             j=i+1
             while j<len(nums):
                 if nums[i]==nums[j]:
