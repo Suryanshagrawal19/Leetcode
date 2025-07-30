20e9a355f9fc0cb3724b9d02dfc8f6dec5f93b85
Suryanshagrawal19 <156478717+Suryanshagrawal19@users.noreply.github.com>
2025-07-30 16:45:25 -0400

    search a 2d matrix

---
+++ Binary Search/74.search-a-2-d-matrix.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row= len(matrix)
+        column= len(matrix[0])
+        
+        top=0
+        bot=row-1
+
+        while top<=bot:
+            row=(top+bot)//2
+            if target>matrix[row][-1]:
+                top=row+1
+            elif target<matrix[row][0]:
+                bot=row-1
+            else:
+                break
+        
+        
+        
+        row = (top+bot)//2
+        l=0
+        r=column-1
+
+        while l<=r:
+            mid=(l+r)//2
+            if target>matrix[row][mid]:
+                l=mid+1
+            elif target<matrix[row][mid]:
+                r=mid-1
+            else:
+                return True
+        return False
