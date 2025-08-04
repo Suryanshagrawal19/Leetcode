6959dfff2c07ab12ff3af315f3e3571ccf51b303
Suryanshagrawal19 <156478717+Suryanshagrawal19@users.noreply.github.com>
2025-08-03 20:41:22 -0400

    linked list cycle

---
+++ Linked_Lists/141.linked-list-cycle.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        temp=set()
+        curr = head
+        while curr:
+            if curr not in temp:
+                temp.add(curr)
+                curr=curr.next
+            elif curr in temp:
+                return True
+        return False
+# @lc code=end
+
